cf07f31cd6c55e25f11e83cbf9753686b47284fb
redkittty <envixtymail@gmail.com>
2024-08-19 19:05:43 -0400

    Made it up-to-date with current build

--- config.h
+++ config.h
@@ -17,8 +17,8 @@ static const int swallowfloating    = 0;     /* 1 means swallow floating windows
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
-static const char font[]            = { "monospace 12" };
-static const char dmenufont[]       = "monospace:size=12";
+static const char font[]            = { "Liberation Mono 12" };
+static const char dmenufont[]       = "Liberation Mono:size=12";
 static const char col_gray1[]       = "#282828";
 static const char col_gray2[]       = "#FBF1C7";
 static const char col_gray3[]       = "#EBDBB2";
@@ -29,8 +29,8 @@ static const char col_blue[]        = "#83A598";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_black, col_blue,  col_blue  },
-  [SchemeHid]  = { col_cyan,  col_gray1, col_cyan  },
+	[SchemeSel]  = { col_black, col_blue,  col_blue  }, 
+   [SchemeHid]  = { col_cyan,  col_gray1, col_cyan  },
 };
 
 /* tagging */
@@ -44,7 +44,7 @@ static const Rule rules[] = {
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-	{ "tabbed",  NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
@@ -80,7 +80,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_blue, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "tabbed", "-r", "2", "st", "-w", "''", NULL };
+static const char *termcmd[]  = { "st", NULL };
 static const char *webcmd[] = { "firefox", NULL };
 static const char *screencmd[] = { "flameshot", "screen", "-n", "1", NULL };
 
